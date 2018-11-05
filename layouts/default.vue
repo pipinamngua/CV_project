16c876f6e5526fdbefd41ac815125ba88a4e1ae9
Nguyen Hoang <nguyen.minh.hoangb@framgia.com>
2018-11-05 17:02:04 +0700

    m

---
+++ layouts/default.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <header id="header">
+            <nav id="main-menu">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About Me</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="contact.html">Get in Touch</a></li>
+                </ul>
+            </nav>
+            <img id="logo"
+                src="img/me.png"
+                alt="This is me.">
+        </header>
+
+        <nuxt />
+
+        <footer id="footer" />
+    </div>
+
+</template>
+
+<style lang="scss">
+    @import '~assets/sass/main.scss';
+    html {
+        font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+            'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+        font-size: 16px;
+        word-spacing: 1px;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: $text-font;
+        color: $text-color;
+
+        @include large-screens {
+            font-size: 125%;
+        }
+    }
+
+    h1,
+    h2,
+    h3,
+    h4 {
+        font-family: $headline-font;
+    }
+
+    p {
+        font-family: $text-font;
+    }
+
+    a {
+        color: $link-color;
+    }
+
+    @include fancy-links;
+
+    #header {
+        @include box(7px);
+        @include box-shadow(2px 0px 4px#999, 1px 1px 6px $secondary-color);
+        height: $header-height;
+        background-color: $theme-color;
+
+        #main-menu {
+            li {
+                display: inline;
+            }
+            a {
+                color: $menu-item-color;
+            }
+        }
+    }
+
+    #footer {
+        height: $footer-height;
+        background-color: $ternary-color;
+    }
+    *,
+    *:before,
+    *:after {
+        box-sizing: border-box;
+        margin: 0;
+    }
+
+    .button--green {
+        display: inline-block;
+        border-radius: 4px;
+        border: 1px solid #3b8070;
+        color: #3b8070;
+        text-decoration: none;
+        padding: 10px 30px;
+    }
+
+    .button--green:hover {
+        color: #fff;
+        background-color: #3b8070;
+    }
+
+    .button--grey {
+        display: inline-block;
+        border-radius: 4px;
+        border: 1px solid #35495e;
+        color: #35495e;
+        text-decoration: none;
+        padding: 10px 30px;
+        margin-left: 15px;
+    }
+
+    .button--grey:hover {
+        color: #fff;
+        background-color: #35495e;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="main"
+    role="main">
+
+    <article id="content">
+      <h1>Welcome to My Site!</h1>
+      <section id="introduction">
+        <h2>Hi, I am Mr. Website!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui pariatur in iure. Rem, aliquid, cumque. Id tenetur, facere aut odio dolores qui, iste sit voluptates ullam doloribus suscipit quasi obcaecati alias voluptatum error ab <a href="#">repellendus</a> deleniti ipsa quae sint! Nobis tempora cupiditate voluptate earum fuga laudantium vitae deleniti consectetur sapiente.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas possimus, et deleniti commodi sit, excepturi! Perspiciatis omnis aut impedit saepe consequatur quis dolores distinctio cumque aperiam velit repellendus dicta temporibus, a ducimus exercitationem, ratione harum quos voluptatem totam possimus eaque, porro quo voluptatum voluptates. Ducimus facere quod laborum, odit numquam amet. Ea sequi, harum neque, alias ratione iure blanditiis, accusamus reprehenderit impedit numquam libero suscipit voluptatibus enim. Exercitationem placeat, repellat.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, eius sapiente excepturi saepe vero maxime! Laborum quidem debitis labore quia magni tempore <a href="#">voluptatibus</a> omnis pariatur consectetur, molestiae nisi et ratione?</p>
+      </section>
+      <section id="promo">
+        <h2>Check Out My Work</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex omnis atque illo odio labore, debitis, qui quo? Odio similique illum eligendi distinctio repellendus quod inventore, aliquam modi, recusandae labore dignissimos aperiam expedita unde ea rem iusto magni ipsum nisi neque!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eum quod et provident consectetur dolores, itaque quae <a href="#">dignissimos voluptatem</a>, accusamus, aut omnis ipsa quidem error magni facilis excepturi explicabo repudiandae. Velit beatae, facere magni. Odio obcaecati possimus reiciendis incidunt quo expedita quis ipsum quibusdam omnis, numquam beatae minima iure tenetur impedit totam ea esse deleniti eum? Minima maiores expedita, accusamus.</p>
+      </section>
+    </article>
+
+    <aside id="sidebar">
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </aside>
+
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '~assets/sass/main.scss';
+  #main {
+      width: $content-width;
+      height: auto;
+      @include large-screens {
+          width: auto;
+          max-width: $content-width;
+      }
+      margin-left: auto;
+      margin-right: auto;
+
+      #content {
+      }
+
+      #sidebar {
+      }
+  }
+  .container {
+      min-height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+  }
+
+  .title {
+      font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+          BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+          sans-serif;
+      display: block;
+      font-weight: 300;
+      font-size: 100px;
+      color: #35495e;
+      letter-spacing: 1px;
+  }
+
+  .subtitle {
+      font-weight: 300;
+      font-size: 42px;
+      color: #526488;
+      word-spacing: 5px;
+      padding-bottom: 15px;
+  }
+
+  .links {
+      padding-top: 15px;
+  }
+</style>
